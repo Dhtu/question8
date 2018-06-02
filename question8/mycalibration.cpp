dcede2c49572cab3ef5f1659b2467d1f4caa0cff
Htu_D <3170102733@zju.edu.cn>
2018-06-02 16:39:01 +0800

    Package the interface

---
+++ question8/mycalibration.cpp
@@ -0,0 +1,10 @@
+#include"stdafx.h"
+using namespace cv;
+
+double mycalibrateCamera(InputArrayOfArrays _objectPoints,
+	InputArrayOfArrays _imagePoints,
+	Size imageSize, InputOutputArray _cameraMatrix, InputOutputArray _distCoeffs,
+	OutputArrayOfArrays _rvecs, OutputArrayOfArrays _tvecs, int flags, TermCriteria criteria)
+{
+	return 0;
+}
---
+++ question8/mycalibration.h
@@ -0,0 +1,16 @@
+#pragma once
+/** @overload double mycalibrateCamera( InputArrayOfArrays objectPoints,
+InputArrayOfArrays imagePoints, Size imageSize,
+InputOutputArray cameraMatrix, InputOutputArray distCoeffs,
+OutputArrayOfArrays rvecs, OutputArrayOfArrays tvecs,
+OutputArray stdDeviations, OutputArray perViewErrors,
+int flags = 0, TermCriteria criteria = TermCriteria(
+TermCriteria::COUNT + TermCriteria::EPS, 30, DBL_EPSILON) )
+*/
+#include "opencv2/calib3d.hpp"
+using namespace cv;
+double mycalibrateCamera(InputArrayOfArrays objectPoints,
+	InputArrayOfArrays imagePoints, Size imageSize,
+	InputOutputArray cameraMatrix, InputOutputArray distCoeffs,
+	OutputArrayOfArrays rvecs, OutputArrayOfArrays tvecs,
+	int flags , TermCriteria criteria);
